993561349ba30559b118d5fef0ef760db98bfb52
Garima <kumargarima59@gmail.com>
2022-07-16 11:24:26 +0530

    style: improved design

--- renderer/components/ChartComponent.jsx
+++ renderer/components/ChartComponent.jsx
@@ -14,32 +14,40 @@ export default function Chart({ packets }) {
   for (let i = 0; i < 8; i++) {
     packetArray.push(
       packets.map((packet, index) => {
-        return { id: index, values: packet[i] };
+        return { id: index, packets: packet[i] };
       })
     );
   }
 
   return (
-    <div className="w-full h-fit border-blue-800 border-2  mt-10">
-      {packetArray.map((packets) => {
+    <div className="w-full h-fit border-blue-800 border-2 mt-20">
+      {packetArray.map((packets, index) => {
         return (
-          <ResponsiveContainer width="100%" aspect={3}>
-            <LineChart
-              data={packets}
-              width={500}
-              height={300}
-              margin={{ top: 40, right: 80, left: 20, bottom: 10 }}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="id" interval={"preserveStartEnd"} />
-              <YAxis />
-              <Tooltip
-                contentStyle={{ backgroundColor: "lightblue", color: "black" }}
-              />
-              <Legend />
-              <Line type="monotone" dataKey="values" stroke="gray" />
-            </LineChart>
-          </ResponsiveContainer>
+          <div className="mt-10">
+            <h1 className="ml-7 font-bold">Channel {index + 1}</h1>
+            <ResponsiveContainer width="100%" aspect={3}>
+              <LineChart
+                data={packets}
+                width={500}
+                height={300}
+                margin={{ top: 40, right: 80, left: 20, bottom: 10 }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="id" interval={"preserveStartEnd"} />
+                <YAxis />
+                <Tooltip
+                  contentStyle={{ backgroundColor: "yellow", color: "black" }}
+                />
+                <Legend />
+                <Line
+                  type="monotone"
+                  dataKey="packets"
+                  stroke="gray"
+                  activeDot={{ stroke: "yellow", strokeWidth: 2, r: 8 }}
+                />
+              </LineChart>
+            </ResponsiveContainer>
+          </div>
         );
       })}
     </div>
