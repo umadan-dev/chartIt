eb16a669055014b614496a500a1fa6fe3064aed2
Garima <kumargarima59@gmail.com>
2022-07-17 20:10:57 +0530

    feat: channel 0 done

--- renderer/components/ChartComponent.jsx
+++ renderer/components/ChartComponent.jsx
@@ -1,55 +1,57 @@
+import { useState, useEffect } from "react";
 import {
   ResponsiveContainer,
   LineChart,
   Line,
   XAxis,
   YAxis,
-  CartesianGrid,
   Legend,
   Tooltip,
 } from "recharts";
 
 export default function Chart({ packets }) {
-  let packetArray = [];
-  for (let i = 0; i < 8; i++) {
-    packetArray.push(
-      packets.map((packet, index) => {
-        return { id: index, packets: packet[i] };
-      })
-    );
-  }
+  const [packetArray, setPacketArray] = useState([]);
+  const [idxx, setIdxx] = useState(-10);
+
+  useEffect(() => {
+    if (idxx < 1000) {
+      //removing this if condition will show graph for all the packets
+      let arr = packets.map((packet, index) => {
+        return { id: idxx + index, values: packet[0] };
+      });
+      setPacketArray([...packetArray, ...arr]);
+      setIdxx((id) => id + 10);
+    }
+  }, [packets]);
+
+  // console.log(packetArray);
 
   return (
     <div className="w-full h-fit border-blue-800 border-2 mt-20">
-      {packetArray.map((packets, index) => {
-        return (
-          <div className="mt-10">
-            <h1 className="ml-7 font-bold">Channel {index + 1}</h1>
-            <ResponsiveContainer width="100%" aspect={3}>
-              <LineChart
-                data={packets}
-                width={500}
-                height={300}
-                margin={{ top: 40, right: 80, left: 20, bottom: 10 }}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="id" interval={"preserveStartEnd"} />
-                <YAxis />
-                <Tooltip
-                  contentStyle={{ backgroundColor: "yellow", color: "black" }}
-                />
-                <Legend />
-                <Line
-                  type="monotone"
-                  dataKey="packets"
-                  stroke="gray"
-                  activeDot={{ stroke: "yellow", strokeWidth: 2, r: 8 }}
-                />
-              </LineChart>
-            </ResponsiveContainer>
-          </div>
-        );
-      })}
+      <div className="mt-10">
+        <h1 className="ml-7 font-bold">Channel 0</h1>
+        <ResponsiveContainer width="100%" aspect={3}>
+          <LineChart
+            data={packetArray}
+            width={500}
+            height={300}
+            margin={{ top: 40, right: 80, left: 20, bottom: 10 }}
+          >
+            <XAxis dataKey="id" interval={"preserveStartEnd"} />
+            <YAxis />
+            <Tooltip
+              contentStyle={{ backgroundColor: "yellow", color: "black" }}
+            />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="values"
+              stroke="gray"
+              activeDot={{ stroke: "yellow" }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 }
