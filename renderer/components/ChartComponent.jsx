c2a6026303b47901f06a24039d193d2efaf7b1b1
Dharmesh <yadavdydv@gmail.com>
2022-07-17 15:56:15 +0530

    feat: Dharmesh

--- renderer/components/ChartComponent.jsx
+++ renderer/components/ChartComponent.jsx
@@ -1,3 +1,32 @@
-export default function Chart() {
-  return null
-}
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+export const Chart = ({ data }) => {
+  return (
+    <>
+      <ResponsiveContainer width="100%" height={200}>
+        <LineChart
+          data={data}
+          syncId="anyId"
+          margin={{
+            top: 30,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <YAxis />
+          <XAxis dataKey="time" />
+          <Line type="monotone" dataKey="value" stroke="#d41152" fill="#000000" />
+        </LineChart>
+      </ResponsiveContainer>
+    </>
+  );
+};
--- renderer/pages/next.tsx
+++ renderer/pages/next.tsx
@@ -1,60 +1,77 @@
-import React, { useState } from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
-const {ipcRenderer} = require('electron')
-import Chart from './../components/ChartComponent';
+import React, { useReducer, useState } from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import { Chart } from './../components/ChartComponent'
+import {
+  dispatchPacket,
+  initState,
+  namesArray,
+  reducer,
+} from '../components/Assets'
+const { ipcRenderer } = require('electron')
 
 function Next() {
-  const [isActive, setActive] = useState(true);
-  const [buttonText, setButtonText] = useState('Fetch Data Stream');
-  const [listenerCount, setCount] = useState(0);
+  const [isActive, setActive] = useState(true)
+  const [buttonText, setButtonText] = useState('Fetch Data Stream')
+  const [listenerCount, setCount] = useState(0)
+  const [state, dispatch] = useReducer(reducer, initState)
 
   const getData = () => {
-    ipcRenderer.on("device-data",(event,packets)=>{
-      console.log(packets)
-      setCount(listenerCount+1)
+    ipcRenderer.on('device-data', (_event, packets) => {
+      dispatchPacket(packets, dispatch)
+      setCount(prevCount => prevCount + 1)
     })
-
-    ipcRenderer.on("fetch-data",(event,message)=>{
-      if(message=='completed'){
+    ipcRenderer.on('fetch-data', (_event, message) => {
+      if (message == 'completed') {
         setActive(true)
         setButtonText('Fetch Data Stream')
       }
-    }) 
+    })
   }
 
   const fetchData = () => {
-    setActive(false);
+    setActive(false)
     ipcRenderer.invoke('fetch-data').then(() => {
       setButtonText('Fetching In Progress')
     })
-    if(listenerCount==0) getData();
-  };
+    if (listenerCount == 0) getData()
+  }
 
   return (
     <React.Fragment>
       <Head>
         <title>Neuphony - Chart It</title>
       </Head>
-      <div className='grid grid-col-1 text-2xl w-full text-center'>
-        <img className='ml-auto mr-auto' src='/images/logo.png' />
+      <div className="grid grid-col-1 text-2xl w-full text-center">
+        <img className="ml-auto mr-auto" src="/images/logo.png" />
         <span>⚡ Render the Graph on this Page ⚡</span>
       </div>
 
-      <div className='mt-4 w-full flex-wrap flex justify-center'>
-      <button className={isActive ? 'btn-blue': 
-      'bg-blue-500 text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed'} 
-      onClick={fetchData}>{buttonText}</button>
+      <div className="mt-4 w-full flex-wrap flex justify-center">
+        <button
+          className={
+            isActive
+              ? 'btn-blue'
+              : 'bg-blue-500 text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed'
+          }
+          onClick={fetchData}
+        >
+          {buttonText}
+        </button>
       </div>
 
-      <div id="chartIt"> 
-      <span className='mt-4 w-full flex-wrap flex justify-center'>⚡  Render Chart Here ⚡</span> 
-      <Chart />
-      </div> 
+      <div id="chartIt">
+        <span className="mt-4 w-full flex-wrap flex justify-center">
+          ⚡ Render Chart Here ⚡
+        </span>
+        {namesArray.map(e => (
+          <Chart key={e} data={state[e]} />
+        ))}
+      </div>
 
-      <div className='mt-10 w-full flex-wrap flex justify-center'>
-        <Link href='/home'>
-          <a className='btn-blue'>Go to home page</a>
+      <div className="mt-10 w-full flex-wrap flex justify-center">
+        <Link href="/home">
+          <a className="btn-blue">Go to home page</a>
         </Link>
       </div>
     </React.Fragment>
