97170e9f4869d25d9c4696a97f3e2a95be72aeab
sharmayatendra <72139503+Yaten96@users.noreply.github.com>
2022-07-17 19:56:44 +0530

    fix: chart component changes

--- renderer/components/ChartComponent.jsx
+++ renderer/components/ChartComponent.jsx
@@ -11,30 +11,40 @@ import {
 
 const { ipcRenderer } = require("electron");
 export default function Chart({ data }) {
-  let packetData = [];
-  for (let i = 0; i < 8; i++) {
-    packetData.push(
-      data?.map((item) => ({ time: item[8], val: Number(item[i]) }))
-    );
-  }
+  const dataArr = data?.map((item) => ({
+    time: item[8],
+    val0: Number(item[0]),
+    val1: Number(item[1]),
+    val2: Number(item[2]),
+    val3: Number(item[3]),
+    val4: Number(item[4]),
+    val5: Number(item[5]),
+    val6: Number(item[6]),
+    val7: Number(item[7]),
+  }));
 
   return (
     <>
-      {packetData.map((item) => (
-        <LineChart
-          width={800}
-          height={300}
-          data={item}
-          margin={{ top: 40, right: 80, left: 10, bottom: 20 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="time" scale="time" />
-          <YAxis type="number" domain={[-20, 20]} scale="linear" />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="val" stroke="#8884d8" />
-        </LineChart>
-      ))}
+      <LineChart
+        width={800}
+        height={300}
+        data={dataArr}
+        margin={{ top: 40, right: 80, left: 10, bottom: 20 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="time" />
+        <YAxis type="number" domain={[-80, 80]} />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="val0" stroke="#36ff39" />
+        <Line type="monotone" dataKey="val1" stroke="#ffbb33" />
+        <Line type="monotone" dataKey="val2" stroke="#e966c5" />
+        <Line type="monotone" dataKey="val3" stroke="#fa8072" />
+        <Line type="monotone" dataKey="val4" stroke="#ff1693" />
+        <Line type="monotone" dataKey="val5" stroke="#f8f7f2" />
+        <Line type="monotone" dataKey="val6" stroke="#FBD604" />
+        <Line type="monotone" dataKey="val7" stroke="#FB0404 " />
+      </LineChart>
     </>
   );
 }
