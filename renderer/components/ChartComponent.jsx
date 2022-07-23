00e5e955fcb2585a613d0d3c9775c5b462e0b867
Hritick Jaiswal <hritickjaiswal22@gmail.com>
2022-07-23 16:17:13 +0530

    First Commit

--- renderer/components/ChartComponent.jsx
+++ renderer/components/ChartComponent.jsx
@@ -1,3 +1,59 @@
-export default function Chart() {
-  return null
+import React, { useEffect, useState } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const fakeData = [
+  { val: "258.46353642294235", time: "3:574" },
+  { val: "261.08971797816156", time: "3:579" },
+  { val: "262.61976566948346", time: "3:583" },
+  { val: "262.83124154936183", time: "3:587" },
+  { val: "262.0132087895336", time: "3:592" },
+  { val: "260.4633868946313", time: "3:596" },
+  { val: "258.1179333553153", time: "3:600" },
+  { val: "254.86134058777975", time: "3:604" },
+  { val: "250.767420961131", time: "3:609" },
+  { val: "245.83401453017143", time: "3:613" },
+];
+
+function Chart({ data }) {
+  const [dataset, setDataset] = useState([]);
+
+  useEffect(() => {
+    setDataset(data.splice(-10));
+  }, [data]);
+
+  return (
+    <main id="main">
+      <ResponsiveContainer width="100%" height="100%" minHeight="600px">
+        <BarChart
+          width={500}
+          height={300}
+          data={dataset}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="time" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="val" fill="#8884d8" />
+        </BarChart>
+      </ResponsiveContainer>
+    </main>
+  );
 }
+
+export default React.memo(Chart);
--- renderer/pages/next.tsx
+++ renderer/pages/next.tsx
@@ -1,34 +1,71 @@
-import React, { useState } from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
-const {ipcRenderer} = require('electron')
-import Chart from './../components/ChartComponent';
+import React, { useEffect, useRef, useState } from "react";
+import Head from "next/head";
+import Link from "next/link";
+const { ipcRenderer } = require("electron");
+import Chart from "./../components/ChartComponent";
+
+function useStateAndRef(initial) {
+  const [value, setValue] = useState(initial);
+  const valueRef = useRef(value);
+  valueRef.current = value;
+  return [value, setValue, valueRef];
+}
 
 function Next() {
   const [isActive, setActive] = useState(true);
-  const [buttonText, setButtonText] = useState('Fetch Data Stream');
+  const [buttonText, setButtonText] = useState("Fetch Data Stream");
   const [listenerCount, setCount] = useState(0);
+  const [data, setData] = useState([]);
+  const [fetchedData, setFetchedData, refFetchedData] = useStateAndRef([]);
+  const timedId = useRef(null);
+
+  function convertTimestamp(timestamp) {
+    const date = new Date(timestamp);
+
+    return `${date.getSeconds()}:${date.getMilliseconds()}`;
+  }
+
+  function objectifyData(packets) {
+    const tempData = [];
+    for (let i = 0; i < packets.length; i++) {
+      const obj = {
+        val: packets[i][0],
+        time: convertTimestamp(packets[i][8]),
+      };
+      tempData.push(obj);
+    }
+
+    setFetchedData((prevData) => [...prevData, ...tempData]);
+  }
 
   const getData = () => {
-    ipcRenderer.on("device-data",(event,packets)=>{
-      console.log(packets)
-      setCount(listenerCount+1)
-    })
+    ipcRenderer.on("device-data", (event, packets) => {
+      objectifyData(packets);
+      setCount(listenerCount + 1);
+    });
 
-    ipcRenderer.on("fetch-data",(event,message)=>{
-      if(message=='completed'){
-        setActive(true)
-        setButtonText('Fetch Data Stream')
+    ipcRenderer.on("fetch-data", (event, message) => {
+      if (message == "completed") {
+        setActive(true);
+        clearInterval(timedId.current);
+        setButtonText("Fetch Data Stream");
       }
-    }) 
+    });
+  };
+
+  function renderGraph() {
+    setData(refFetchedData.current);
   }
 
   const fetchData = () => {
     setActive(false);
-    ipcRenderer.invoke('fetch-data').then(() => {
-      setButtonText('Fetching In Progress')
-    })
-    if(listenerCount==0) getData();
+    ipcRenderer.invoke("fetch-data").then(() => {
+      setButtonText("Fetching In Progress");
+    });
+    if (listenerCount == 0) {
+      getData();
+      timedId.current = setInterval(renderGraph, 1000);
+    }
   };
 
   return (
@@ -36,29 +73,38 @@ function Next() {
       <Head>
         <title>Neuphony - Chart It</title>
       </Head>
-      <div className='grid grid-col-1 text-2xl w-full text-center'>
-        <img className='ml-auto mr-auto' src='/images/logo.png' />
+      <div className="grid grid-col-1 text-2xl w-full text-center">
+        <img className="ml-auto mr-auto" src="/images/logo.png" />
         <span>⚡ Render the Graph on this Page ⚡</span>
       </div>
 
-      <div className='mt-4 w-full flex-wrap flex justify-center'>
-      <button className={isActive ? 'btn-blue': 
-      'bg-blue-500 text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed'} 
-      onClick={fetchData}>{buttonText}</button>
+      <div className="mt-4 w-full flex-wrap flex justify-center">
+        <button
+          className={
+            isActive
+              ? "btn-blue"
+              : "bg-blue-500 text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed"
+          }
+          onClick={fetchData}
+        >
+          {buttonText}
+        </button>
       </div>
 
-      <div id="chartIt"> 
-      <span className='mt-4 w-full flex-wrap flex justify-center'>⚡  Render Chart Here ⚡</span> 
-      <Chart />
-      </div> 
+      <div id="chartIt">
+        <span className="mt-4 w-full flex-wrap flex justify-center">
+          ⚡ Render Chart Here ⚡
+        </span>
+        <Chart data={data} />
+      </div>
 
-      <div className='mt-10 w-full flex-wrap flex justify-center'>
-        <Link href='/home'>
-          <a className='btn-blue'>Go to home page</a>
+      <div className="mt-10 w-full flex-wrap flex justify-center">
+        <Link href="/home">
+          <a className="btn-blue">Go to home page</a>
         </Link>
       </div>
     </React.Fragment>
-  )
+  );
 }
 
-export default Next
+export default Next;
