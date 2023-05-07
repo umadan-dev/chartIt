c3521657c7a129eb0e8751c34274c1f7613eaf85
am0dh <amodhmandloi2@gmail.com>
2023-05-07 14:13:12 +0530

    Added functionality for Live Chart

--- renderer/components/ChartComponent.jsx
+++ renderer/components/ChartComponent.jsx
@@ -1,3 +1,34 @@
-export default function Chart() {
-  return null
+import {
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  Tooltip
+} from "recharts";
+
+export default function Chart({ data }) {
+  const dataArray = data?.map((item) => ({
+    time: item[8],
+    value0: Number(item[0]),
+  }));
+
+  return (
+    <>
+      <LineChart
+        data={dataArray}
+        width={700}
+        height={500}
+        margin={30}
+      >
+        <CartesianGrid strokeDasharray="2 2" />
+        <XAxis dataKey="time" />
+        <YAxis type="number" domain={[-100, 100]} />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="value0" stroke="#32a868" />
+      </LineChart>
+    </>
+  );
 }
